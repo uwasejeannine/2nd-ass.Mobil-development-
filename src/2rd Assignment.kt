93dda964db3ef7b2e8ca55a9c9af821c2aa92b9d
uwasejeannine <jeannineuwasee@gmail.com>
2021-02-28 12:27:23 +0300

    2rd Assignment

---
+++ src/2rd Assignment.kt
@@ -0,0 +1,31 @@
+fun main() {
+    name()
+    var result=modulus(30,4)
+    println(result)
+    addition()
+    InterestingFact()
+}fun name(){
+    var name="jeannine Uwase"
+    println(name)
+}
+fun num (){
+    var num1=30
+    var num2=4
+}
+fun modulus (num1:Int,num2:Int):Int{
+    var modulus=num1%num2
+    return modulus
+}
+
+fun addition(){
+    var num1=4
+    var num2=5
+    var num3=6
+    var num4=7
+    var sum=num1+num2+num3+num4
+    println(sum)
+}
+fun InterestingFact(){
+    var InterestingFact="excellence"
+    println(InterestingFact)
+}
